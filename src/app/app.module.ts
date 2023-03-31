6628e8ca33cc4784e4748661c3d3a6038451fd64
Moneer <moneer.alromimah@gmail.com>
2023-03-30 22:55:24 -0600

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HeroSectionComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
